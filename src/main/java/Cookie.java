a03a44c8e91ce80807379c28ef1f9366cb576796
Vincent-Delmotte <vincent.delmotte@etu.unice.fr>
2020-10-06 18:33:35 +0200

    Création classe Recipe

--- src/main/java/Cookie.java
+++ src/main/java/Cookie.java
@@ -1,20 +1,21 @@
 /** Represents a Cookie
  * @author Delmotte Vincent
  */
-
 public class Cookie {
 
     protected String name;
     protected int price;
+    protected Recipe recipe;
 
     /**
      * Creates a cookie with a name and a price
      * @param name cookie's name
      * @param price cookie"s price
      */
-    public Cookie(String name, int price) {
+    public Cookie(String name, int price, Recipe recipe) {
         this.name = name;
         this.price = price;
+        this.recipe = recipe;
     }
 
     public String getName() {
@@ -31,5 +32,13 @@ public class Cookie {
 
     public void setPrice(int price) {
         this.price = price;
+    }
+
+    public Recipe getRecipe() {
+        return recipe;
+    }
+
+    public void setRecipe(Recipe recipe) {
+        this.recipe = recipe;
     }
 }
