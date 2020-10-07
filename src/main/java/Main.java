5f5a20a6b47344c17a27acea3d81eb42ee61924b
Vincent-Delmotte <vincent.delmotte@etu.unice.fr>
2020-10-07 14:32:58 +0200

    correction topping #1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,5 +5,6 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
         new Recipe(Dough.CHOCOLATE, Flavor.VANILLA, new ArrayList<Topping>(), Mix.MIXED, Cooking.CHEWY);
+        //change
     }
 }
