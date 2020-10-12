5e32a039dae91a34b4e68b45a389f725392554b5
Vincent-Delmotte <vincent.delmotte@etu.unice.fr>
2020-10-12 11:39:20 +0200

    ajout classe Ingredient

---
+++ src/main/java/Ingredient.java
@@ -0,0 +1,10 @@
+public class Ingredient {
+
+    protected String name;
+    protected int price;
+
+    public Ingredient(String name, int price) {
+        this.name = name;
+        this.price = price;
+    }
+}
