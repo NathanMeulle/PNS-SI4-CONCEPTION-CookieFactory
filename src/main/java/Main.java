3a1603c54dc99ab840ddb26d7716a8941a963197
Vincent-Delmotte <vincent.delmotte@etu.unice.fr>
2020-10-12 11:51:31 +0200

    Supression enum

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,8 +4,11 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        new Recipe(Dough.CHOCOLATE, Flavor.VANILLA, new ArrayList<Topping>(3), Mix.MIXED, Cooking.CHEWY);
-        //change
+        Cookie choco = new Cookie("Chocolate");
+        Manager manager = new Manager(10,"Nathan");
+        Store store = new Store(1,"adresse", 10,"06.06.06.06.06","nathan.meulle@gmail.com", manager);
+        Order order = new Order(store);
+        order.addCookie(choco,1);
     }
     
 }
