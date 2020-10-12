b62fa399bbd008321486d9008373e2aa3cb1887e
Thibaut Esteve <estevet@hotmail.fr>
2020-10-12 11:48:44 +0200

    #1 Ajout de Schedule et de OrderHistory à Store

---
+++ src/main/java/OrderHistory.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+public class OrderHistory {
+
+    private ArrayList<Order> listOrder;
+
+    public OrderHistory() {}
+
+    public void addOrder(Order order){
+        listOrder.add(order);
+    }
+
+    public void generateStats() {}
+
+
+}
