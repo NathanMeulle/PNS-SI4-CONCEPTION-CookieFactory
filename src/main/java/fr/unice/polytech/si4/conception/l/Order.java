7fd8533bf3f7fd35cedea34fcbb4a819c84ff7cb
Vincent-Delmotte <vincent.delmotte@etu.unice.fr>
2020-10-16 16:43:14 +0200

    ajout getter setter order et ajout attribut #1

--- src/main/java/fr/unice/polytech/si4/conception/l/Order.java
+++ src/main/java/fr/unice/polytech/si4/conception/l/Order.java
@@ -14,14 +14,20 @@ public class Order {
     protected int price;
     protected int nbOrder;
     protected HashMap<Cookie, Integer> cookies;
+    protected AnonymousCustomer anonymousCustomer;
 
-    public Order(Store store) {
+    public Order(Store store, AnonymousCustomer anonymousCustomer) {
         this.idOrder = generateIdOrder();
         this.store = store;
         this.date = new Date();
         this.cookies = new HashMap<>();
+        this.anonymousCustomer = anonymousCustomer;
     }
 
+    /**
+     * Generate a unique id that permits to recognize an order
+     * @return id
+     */
     private int generateIdOrder() {
         return 1000;
     }
@@ -47,5 +53,61 @@ public class Order {
         cookies.forEach((cookie,quantity) -> {
             this.price += cookie.getPrice() * quantity;
         });
+    }
+
+    public int getIdOrder() {
+        return idOrder;
+    }
+
+    public void setIdOrder(int idOrder) {
+        this.idOrder = idOrder;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public Store getStore() {
+        return store;
+    }
+
+    public void setStore(Store store) {
+        this.store = store;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getNbOrder() {
+        return nbOrder;
+    }
+
+    public void setNbOrder(int nbOrder) {
+        this.nbOrder = nbOrder;
+    }
+
+    public HashMap<Cookie, Integer> getCookies() {
+        return cookies;
+    }
+
+    public void setCookies(HashMap<Cookie, Integer> cookies) {
+        this.cookies = cookies;
+    }
+
+    public AnonymousCustomer getAnonymousCustomer() {
+        return anonymousCustomer;
+    }
+
+    public void setAnonymousCustomer(AnonymousCustomer anonymousCustomer) {
+        this.anonymousCustomer = anonymousCustomer;
     }
 }
