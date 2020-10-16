2bf49588f62c2283f100027212abc40929202576
NathanMeulle <54913885+NathanMeulle@users.noreply.github.com>
2020-10-16 22:55:46 +0200

    Corrections Kitchen, ajout doc, ajout todo... #1

--- src/main/java/fr/unice/polytech/si4/conception/l/Kitchen.java
+++ src/main/java/fr/unice/polytech/si4/conception/l/Kitchen.java
@@ -1,10 +1,14 @@
 package fr.unice.polytech.si4.conception.l;
 
 
+import fr.unice.polytech.si4.conception.l.exceptions.OutOfStock;
+
 import java.util.HashMap;
-import java.util.List;
 
-
+/**
+ * Class representing the kitchen in charge of stocks, receiving orders and preparing cookies
+ * @author Thibaut
+ */
 public class Kitchen {
 
     static private HashMap<Ingredient, Integer> stock;
@@ -16,57 +20,63 @@ public class Kitchen {
 
     /**
      * Method to know if all cookies in an order are feasible or not
+     *
      * @param cookieList : A hashmap representing the cookies to be realized with their number to realize
      * @return : a boolean indicating true if all cookies are feasible and false
-     *           if at least one cookie is not feasible
+     * if at least one cookie is not feasible
      */
-    static public boolean achievableCookie(HashMap<Cookie, Integer> cookieList){
-        for(Cookie c : cookieList.keySet()){           //On itère sur chaque cookie de la HashMap cookieList
-            for(int i=0; i < cookieList.get(c); i++){  //Puis pour chaque cookie on boucle sur le nombre indiqué dans cookielist
-                if(!canDo(c.ingredients)){      // On vérfie si les ingrédient du cookie sont disponible en stock avec la méthode canDo
+    static public boolean achievableCookie(HashMap<Cookie, Integer> cookieList) {
+        for (Cookie c : cookieList.keySet()) {           //On itère sur chaque cookie de la HashMap cookieList
+            for (int i = 0; i < cookieList.get(c); i++) {  //Puis pour chaque cookie on boucle sur le nombre indiqué dans cookielist
+                if (!canDo(c)) {      // On vérfie si les ingrédients du cookie sont disponibles en stock avec la méthode canDo
                     return false;   // Et on retourne false si ce n'est pas le cas.
-                }
+                } // TODO : A CORRIGER : don't work : ici la quantité n'est pas décrémentée des stocks, on ne peut donc pas savoir si l'on peut faire 2 cookies
             }
         }
         return true;  //Sinon, on retourne true
     }
 
     /**
-     * Indicate if all the ingredient of a recipe are present in the stock and if there are in quantity
-     * @param Recipe : the list of ingredients of the recipe
+     * Indicate if all the ingredients of a cookie are present in the stock and if there are in quantity
+     *
+     * @param cookie : a cookie
      * @return a boolean with true if the recipe is feasible and false in contrary
      */
-    static public boolean canDo(List<Ingredient> Recipe) {
-
-        for(Ingredient i : Recipe){
-
-            try{
+    static public boolean canDo(Cookie cookie) {
+        for (Ingredient i : cookie.getIngredients()) {
+            try {
                 sufficientQuantity(i);
-            } catch (Exception e){
-                e.getMessage();
+            } catch (Exception e) {
+                Log.add(e.getMessage());
+                return false;
             }
         }
-
         return true;
     }
 
     /**
      * Indicate if a ingredient is in sufficient quantity or not
+     *
      * @param ingredient : ingredient to evaluate
      * @return : a boolean with true if the ingredient is in sufficient quantity and false in contrary
      */
     static void sufficientQuantity(Ingredient ingredient) throws OutOfStock {
-        if(!(getQuantity(ingredient) > 0)){
+        if (getQuantity(ingredient) <= 0) {
             throw new OutOfStock(String.format("Ingredient : %s is out of stock", ingredient.getName()));
         }
     }
 
     /**
-     * Get the quantity of an igredient in the stock
+     * Get the quantity of an ingredient in the stock
+     *
      * @param ingredient : ingredient to evaluate
      * @return : an int of the quantity of the ingredient
      */
-    static int getQuantity(Ingredient ingredient){
+    static int getQuantity(Ingredient ingredient) {
         return stock.get(ingredient);
     }
 }
+
+
+
+//TODO : other methods ?
