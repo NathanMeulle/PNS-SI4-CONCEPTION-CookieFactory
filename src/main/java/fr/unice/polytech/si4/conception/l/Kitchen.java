0a6ac8e730c7077ed540e9ff4baddd3d60fcb4bf
Lemuel-DONTO <55319038+Lemouz-NALMOK@users.noreply.github.com>
2020-10-17 18:52:29 +0200

    Add Method Cook And Manage Stock Methods #6

--- src/main/java/fr/unice/polytech/si4/conception/l/Kitchen.java
+++ src/main/java/fr/unice/polytech/si4/conception/l/Kitchen.java
@@ -17,6 +17,21 @@ public class Kitchen {
         stock = new HashMap<>();
     }
 
+    /**
+     * Method to know if all ingredients and their quantity
+     *
+     * @param cookieList : A hashmap representing the cookies to be realized with their number to realize
+     * @return : a HasMap of all ingredients and the required quantity
+     */
+    private HashMap<Ingredient, Integer> requireIngredients(HashMap<Cookie, Integer> cookieList){
+        HashMap<Ingredient, Integer> requests = new HashMap<>();
+        for (Cookie c : cookieList.keySet()) {
+            for (Ingredient i : c.getIngredients()) {
+                requests.put(i, cookieList.get(c));
+            }
+        }
+        return requests;
+    }
 
     /**
      * Method to know if all cookies in an order are feasible or not
@@ -25,23 +40,8 @@ public class Kitchen {
      * @return : a boolean indicating true if all cookies are feasible and false
      * if at least one cookie is not feasible
      */
-    public boolean achievableCookie(HashMap<Cookie, Integer> cookieList) {
-        HashMap<Ingredient, Integer> requests = new HashMap<>();
-        for (Cookie c : cookieList.keySet()) {
-            for (Ingredient i : c.getIngredients()) {
-                requests.put(i, cookieList.get(c));
-            }
-        }
-        return canDo(requests);
-    }
-
-    /**
-     * Indicate if all the ingredients of a cookie are present in the stock and if there are in quantity
-     *
-     * @param ingredientsMap : a cookie
-     * @return a boolean with true if the recipe is feasible and false in contrary
-     */
-    public boolean canDo(HashMap<Ingredient, Integer> ingredientsMap) {
+    public boolean canDo(HashMap<Cookie, Integer> cookieList) {
+        HashMap<Ingredient, Integer> ingredientsMap = requireIngredients(cookieList);
         for (Ingredient i : ingredientsMap.keySet()) {
             try {
                 sufficientQuantity(i, ingredientsMap.get(i));
@@ -59,9 +59,26 @@ public class Kitchen {
      * @param ingredient : ingredient to evaluate
      * @return : a boolean with true if the ingredient is in sufficient quantity and false in contrary
      */
-    void sufficientQuantity(Ingredient ingredient, int n) throws OutOfStockException {
-        if (getQuantity(ingredient) < n) {
+    void sufficientQuantity(Ingredient ingredient, int nbre) throws OutOfStockException {
+        if (getQuantity(ingredient) < nbre) {
             throw new OutOfStockException(String.format("Ingredient : %s is out of stock", ingredient.getName()));
+        }
+    }
+
+    void decrementStock(Ingredient ingredient, int nbre){
+        if(this.stock.get(ingredient) > nbre)
+            this.stock.put(ingredient, (this.stock.get(ingredient) - nbre));
+        else
+            this.stock.put(ingredient, 0);
+    }
+
+    void setStock(Ingredient ingredient, int n){
+        this.stock.put(ingredient, this.stock.get(ingredient) + n);
+    }
+
+    void setStock(HashMap<Ingredient, Integer> ingredientList){
+        for (Ingredient i : ingredientList.keySet()) {
+            this.stock.put(i, this.stock.get(i) + ingredientList.get(i));
         }
     }
 
@@ -75,8 +92,13 @@ public class Kitchen {
         return stock.get(ingredient);
     }
 
+    void cook(HashMap<Cookie, Integer> cookieList){
+        HashMap<Ingredient, Integer> ingredientsMap = requireIngredients(cookieList);
+        for (Ingredient i : ingredientsMap.keySet()) {
+            decrementStock(i, ingredientsMap.get(i));
+        }
+    }
+
 }
 
 
-
-//TODO : other methods ?
