b7adf88b5cff43c2a7c70d3c2dfd919546d49f2f
Thibaut Esteve <estevet@hotmail.fr>
2020-10-25 15:04:28 +0100

    #4 Complétion des test de CookieFactory

--- src/test/java/fr/unice/polytech/si4/conception/l/CookieFactoryTest.java
+++ src/test/java/fr/unice/polytech/si4/conception/l/CookieFactoryTest.java
@@ -1,16 +1,41 @@
 package fr.unice.polytech.si4.conception.l;
 
+import fr.unice.polytech.si4.conception.l.exceptions.AlreadyCreatedException;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class CookieFactoryTest {
 
-    @Test
-    void subscription() {
+    String name;
+    String phoneNumber;
+    String mail;
+    CookieFactory cookieFactory;
+    Log log;
+
+    @BeforeEach
+    void setUp() {
+        name = new String("Esteve");
+        phoneNumber = new String("0658601237");
+        mail = new String("estevet@hotmail.fr");
+        cookieFactory = new CookieFactory(null, null);
+        log = new Log();
     }
 
     @Test
-    void addCustomer() {
+    void subscription() {
+        try{
+            cookieFactory.subscription(name, phoneNumber, mail);
+        } catch (AlreadyCreatedException exception){
+            //log.add("Customer already exist");
+        }
+
+        Customer customerSubscribe = cookieFactory.getCustomerByMail("mail");
+
+        assertEquals(name, customerSubscribe.getName());
+        assertEquals(phoneNumber, customerSubscribe.getPhoneNumber());
+        assertEquals(mail, customerSubscribe.getMail());
     }
+
 }
